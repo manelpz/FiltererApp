fc6caa15d9b55a2b4a2775c7621e1572f82aa052
Emmanuel López Guerrero <manelpz@icloud.com>
2020-06-15 22:50:27 -0500

    Adding UISlider values

--- Filterer/Filterer/ViewController.swift
+++ Filterer/Filterer/ViewController.swift
@@ -26,20 +26,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     @IBOutlet var compareButton: UIButton!
     @IBOutlet var OriginalImageLabel: UILabel!
     
+    @IBAction func onSlider(_ sender: UISlider) {
+        filterSlider.value = roundf(filterSlider.value)
+    }
+    
     @IBAction func onFilter(_ sender: UIButton) {
         if (sender.isSelected){
             hiddenMenu()
-            //EditButton.isEnabled = false
             sender.isSelected = false
         }else{
-           // EditButton.isEnabled = true
             showSecondaryMenu()
+            hiddenSliderMenu()
+            EditButton.isSelected = false
             sender.isSelected = true
         }
     }
     
     @IBAction func onEdit(_ sender: UIButton) {
-       //view.addSubview(sliderMenu)
          if (sender.isSelected){
             hiddenMenu()
             hiddenSliderMenu()
@@ -47,7 +50,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         }else{
             hiddenMenu()
             showSliderMenu()
-            
+            filterButton.isSelected = false
             sender.isSelected = true
         }
     }
@@ -55,6 +58,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     func showSliderMenu(){
         view.addSubview(sliderMenu)
         sliderMenu.translatesAutoresizingMaskIntoConstraints = false
+        sliderMenu.backgroundColor = UIColor.white.withAlphaComponent(0.5)
         let bottomConstraint = sliderMenu.bottomAnchor.constraint(equalTo: buttonMenu.topAnchor)
         let leftConstraint = sliderMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
         let rightConstraint = sliderMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
@@ -62,9 +66,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         NSLayoutConstraint.activate([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
         
         view.layoutIfNeeded()
-        
         self.sliderMenu.alpha = 0
-        
         UIView.animate(withDuration: 0.4){
             self.sliderMenu.alpha = 1.0
         }
@@ -82,17 +84,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
     
     @IBAction func onNewPhoto(_ sender: Any) {
         let actionSheet = UIAlertController(title: "New photo", message: nil, preferredStyle: .actionSheet)
-        
         actionSheet.addAction(UIAlertAction(title:"Camera", style: .default, handler: {action in
             self.showCamera()
         }))
-        
         actionSheet.addAction(UIAlertAction(title: "Album", style: .default, handler: {action in
             self.showAlbum()
         }))
-        
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
         self.present(actionSheet, animated: true, completion: nil)
     }
     
@@ -100,7 +98,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         let cameraPicker = UIImagePickerController()
         cameraPicker.delegate = self
         cameraPicker.sourceType = .camera
-        
         present(cameraPicker, animated: true, completion: nil)
     }
     
@@ -108,7 +105,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         let cameraPicker = UIImagePickerController()
         cameraPicker.delegate = self
         cameraPicker.sourceType = .photoLibrary
-        
         present(cameraPicker, animated: true, completion: nil)
     }
 
@@ -146,18 +142,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
             self.imageViewfiltered.alpha = 0
         }
     }
+    
     func showSecondaryMenu(){
         view.addSubview(secundaryMenu)
         let bottomConstraint = secundaryMenu.bottomAnchor.constraint(equalTo: buttonMenu.topAnchor)
         let leftConstraint = secundaryMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
         let rightConstraint = secundaryMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
-        let heightConstraint =  secundaryMenu.heightAnchor.constraint(equalToConstant: 44)
+        let heightConstraint =  secundaryMenu.heightAnchor.constraint(equalToConstant: 54)
         NSLayoutConstraint.activate([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
         
         view.layoutIfNeeded()
-        
         self.secundaryMenu.alpha = 0
-        
         UIView.animate(withDuration: 0.4){
             self.secundaryMenu.alpha = 1.0
         }
@@ -203,7 +198,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
             imageViewfiltered.image = filteredImage
             sender.isSelected = true
         }
-        
     }
     
     /*SecondaryPurpleFilter*/
@@ -213,10 +207,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         let PhotoFilter = Photo()
         let ColorDictionary = PhotoFilter.ReadColors(imageParam: images!)
         
-       
-            filteredImage = PhotoFilter.FilterImageDark(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
+        filteredImage = PhotoFilter.FilterImageDark(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
         
-         imageView.image = filteredImage
+        imageView.image = filteredImage
         compareButton.isEnabled = true
         EditButton.isEnabled = true
     }
@@ -228,7 +221,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,UINaviga
         let myRGBA = RGBAImage(image: images!)
         let PhotoFilter = Photo()
         let ColorDictionary = PhotoFilter.ReadColors(imageParam: images!)
-        
         filteredImage = PhotoFilter.FilterImageBright(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
         
         imageView.image = filteredImage
@@ -243,7 +235,6 @@ compareButton.isEnabled = true
         let myRGBA = RGBAImage(image: images!)
         let PhotoFilter = Photo()
         let ColorDictionary = PhotoFilter.ReadColors(imageParam: images!)
-        
         filteredImage = PhotoFilter.FilterImageTemperatureCold(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
         
         imageView.image = filteredImage
@@ -258,42 +249,32 @@ compareButton.isEnabled = true
         let myRGBA = RGBAImage(image: images!)
         let PhotoFilter = Photo()
         let ColorDictionary = PhotoFilter.ReadColors(imageParam: images!)
-        
         filteredImage = PhotoFilter.FilterImageTemperatureHot(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
         
         imageView.image = filteredImage
-compareButton.isEnabled = true
+        compareButton.isEnabled = true
         EditButton.isEnabled = true
     }
-   
     
     /*SecondaryRedFilter*/
     @IBAction func onSecondaryRedFilter(_ sender: UIButton) {
-
         let images = imageView.image
         let myRGBA = RGBAImage(image: images!)
         let PhotoFilter = Photo()
         let ColorDictionary = PhotoFilter.ReadColors(imageParam: images!)
-        
         filteredImage = PhotoFilter.FilterImageContrast(Level: LevelFilter.medium, avgColor: ColorDictionary, myRGBA: myRGBA!)
         
         imageView.image = filteredImage
         compareButton.isEnabled = true
-        //sender.isSelected = true
         EditButton.isEnabled = true
     }
-    
     
     /*share*/
     @IBAction func onShare(_ sender: AnyObject) {
             let activityController = UIActivityViewController(activityItems:["Check out my first app", imageView.image!], applicationActivities: nil)
     present(activityController, animated: true, completion: nil)
     }
-    
-
 }
-
-
 
 protocol FilterType {
     func FilterImageContrast(Level: LevelFilter, avgColor: Dictionary<String,Int>, myRGBA: RGBAImage) -> UIImage
