6091a90cbdd3b93d7a754c7e2cd5c129a4846eed
emmanuel lopez <emmanuel.lopez@simplias.com>
2021-03-02 13:35:26 -0600

    Adding project FilterWatch

---
+++ Filterer/FilterWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FilterWatch WatchKit Extension
+//
+//  Created by Emmanuel Lopez Guerrero on 02/03/21.
+//  Copyright © 2021 Emmanuel Lopez Guerrero. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
