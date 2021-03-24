bef98efe19a4d7de2b88d11c316ee3338159619a
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-03-23 22:43:43 -0600

    .

--- Filterer/FilterWatch WatchKit Extension/InterfaceController.swift
+++ Filterer/FilterWatch WatchKit Extension/InterfaceController.swift
@@ -43,7 +43,7 @@ class InterfaceController: WKInterfaceController {
     
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+        // This method is called when watch 
         super.didDeactivate()
     }
     
